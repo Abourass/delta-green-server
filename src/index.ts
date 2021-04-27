1752e1360ae89531067627455087d9703b5788a7
Abourass <abourassa@assetval.com>
2021-04-27 10:32:30 -0600

    :art: Signed

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,90 @@ import "./styles/Tailwind.css";
 
 const app = new App({ target: document.body });
 
+console.log(`
+                                                          :i-1tncYLZmqpdddddddpqmZLYcnt1-i:
+                                                  .,~1xYOpdddddddddddddddddddddddddddddddddpOYx1~,.
+                                              :?jYZdddddddddddddddddddddddddddddddddddddddddddddddZYj?:
+                                         .l)zZdddddddddddddddddddddddddddddddddddddddddddddddddddddddddZz)l.
+                                      ,[cZdddddddddddddddddddddwQXur/([[}???]][(/ruXQwdddddddddddddddddddddZc["
+                                   :|CqddddddddddddddddwYf{1>,I/[\`   )0OLOc!  ,OQCr:  '!}{[fYwddddddddddddddddqC|:
+                                ,|LddddddddddddddpQr?!".  :pc:~LZ<  _p(. !Xu; +q}]m{  [Oxvr  ."!?rQpddddddddddddddL|,
+                             ']YqdddddddddddddY{!^        "qmYnU0X, _p|  lXv: {qqm-'  +LL{'        ^!{YdddddddddddddqY]\`
+                           ;fZddddddddddddQ(i\`            "On/pf}01' }Z0LZc!  uqImv'.<];tw+            \`i(QddddddddddddZf;
+                        .!cwddddddddddpJ[,.               "0n\`-<.I;.':i_[?++~><i'"I^ IxOZ/\`               .,[Jpddddddddddwc!.
+                       IUpddddddddddO{,.+<.;<             .i[rYL0mqpdddddddddddddpqm0LYr[l.             >;.>+.,{OddddddddddpUI
+                     lCdddddddddddx;.   -QwQ,        "?vQZpddddddddddddwx<l~rwddddddddddddpZQv?"        ,Jw0?   .;xdddddddddddJl
+                   ,cpdddddddddm[^    \`>[Ymur>   :|CmpddddddddddddddddQ,     ,LddddddddddddddddpmC|:   >xvmJ1~\`    ^[mdddddddddpc"
+                 \`|mdddddddddQ-'         ~>'  +Umpdddddddddddddddddddpj       jpdddddddddddddddddddpmU+  .>+         '-Ldddddddddm|\`
+                <Jddddddddd0]'            .?Cqdddddddddddddddddddddddd0I     I0ddddddddddddddddddddddddqY?.            ']0dddddddddJ<
+              ^(qddddddddm)'            lvwddddddddddddddddddddddddddddCl   ICddddddddddddddddddddddddddmdwvl            ')mddddddddq(^
+             :Udddddddddv\`           '-QpdddddddddddddddddddddddddddddZ_     ~Zddddddddddddddddddddddddmddddp0-'           \`vdddddddddU:
+            <qddddddddwl           .[wdddddddddddddddddddddddddddddddw!  '^'  lwddddddddddddddddddddddpwdddddddw[.           lwddddddddq>
+           )dddddddddr'           ]pddddddddddddddddddddddddddddddddq,  \`|q/^  "wdddddddddddddddddddddmddddddddddq]           .rddddddddd)
+          uddddddddq+.          >wdddddddddddddddddddddddddddddddddZ\`  ^/dddt^  .Qdddddddddddddddddddwpddddddddddddm>          .+qddddddddu
+        .zpdddddddC!          "XpdddddddddddddddddddddddddddddddddJ^  ^jdddddn^  ^YddddddddddddddddddmddddddddddddddpX"          !Cdddddddpz.
+       'cpdddddddcI...Ui     ]Oddddddddddddddddddddddddddddddddddv,  'cdddddddX\`  "uddddddddddddddddmpddddddddddddddddO[     IU...Icdddddddpc'
+      'npdddddddu, "YpdZx+ ^jpddddddddddddddddddddddddddddddddddn,  .CdddddddddL.  ,rdddddddddddddddwpdddddddddddddddddpj^ >t0ddJ, ,udddddddpn'
+     ./qdddddddv"  lJYw)  lXdddddddddddddddddddddddddddddddddddj"  ^OdddddddddddZ^  "fpddddddddddddwpddddddddddddddddddddXl  1qJJl  "vdddddddq/.
+     [mdddddddX,       > lCddddddddddddddddddddddddddddddddddpf\`  ;OdddddddddddddZI  \`tqddddddddddqqddddddddddddddddddddddQ! _.      ,Xdddddddm[
+    >CdddddddL;         lmpwddddddddddddddddddddddddddddddddqt.  i0ddddddddddddddd0<  ./wdddddddddwpdddddddddddddddddddddddml         ;LdddddddC<
+   ,ndddddddm>         :mddpqddddddddddddddddddddddddddddddw|   +Qddddddddddddddddd0-   (wdddddddqqdddddddddddddddddddddddddm:         >mdddddddn,
+  .]pddddddd1         \`mddddqqddddddddddddddddddddddddddddw1  .?0dddddddddddddddddddO].  }wddddddqqddddddddddddddddddddddddddm\`         1dddddddp].
+  ;CdddddddJ         'Jddddddqqddddddddddddddddddddddddddq]  .]OdddddddddddddddddddddZ[.  _qddddqqddddddddddddddddddddddddddddJ'         JdddddddCI
+ ')dddddddp;         fpdddddddpqddddddddddddddddddddddddw>  '[wdddddddddddddddddddddddw}'  iwddpqpddddddddddddddddddddddddddddpf         ;pddddddd)'
+ ,0dddddddf         ~mdddddddddpwddddddddddddddddddddddZl  '}qdddddddddddddddddddddddddp1'  lOdpwddddddddddddddddddddddddddddddm~         fddddddd0,
+.]dddddddq,        'XddddddddddddmddddddddddddddddddddQl  ')ddddddddddddddddddddddddddddd('  ILwddddddddddddddddddddddddddddddddY'        :qddddddd[.
+.Xdddddddr'        ?ZdddddddddddddmpdddddddddddddddddJl  .tdddddddddddddddddddddddddddddddj.  ;XddddddddddddddddddddddddddddddddZ?        'rdddddddX.
+"pddddddd+.       'vpddddddddddddddwpdddddddddddddddXl  'ndddddddddddddddddddddddddddddddddv'  Izdddddddddddddddddddddddddddddddpv'       .+dddddddp"
+[ddddddd0;        >CddddddddddddddddwpddddddddddddpcI  \`zdddddddddddddddddddddddddddddddddddX^  ;upddddddddddddddddddddddddddddddC>        :Qddddddd[
+rdddddddx^  i\`    )mdddddddddddddddddqpddddddddddpu:  ,Yddddddddddddddddddddddddddddddddddddpc,  ,npdddddddddddddddddddddddddddddm)    'l  ^xdddddddr
+Lddddddd)\`.:zU11; rdddddddddddddddddddqqddddddddqn"  IUdddddddddddddddddddddddddddddddddddddwpJl  ^xqdddddddddddddddddddddddddddddr :}}Yz, \`)dddddddL
+wddddddd-^]rpdQ~ ,uwwwwwwwwwwwwwwwwwwwwZmwwwwwwmj\`  !XwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwOqwYi  'fmwwwwwwwwwwwwwwwwwwwwwwwwwwwwu, ~Qdpu]^-dddddddw
+dddddddd>   U~<_^lYdddddddddddddddddddddqqddddpj.  >JddddddddddddddddddddddddddddddddddddddZddddC~  .tpdddddddddddddddddddddddddddYl^-+_J   >dddddddd
+dddddddw>   .    !Uddddddddddddddddddddddqpddd(  .~LddddddddddddddddddddddddddddddddddddddpwdddddQ+.  1dddddddddddddddddddddddddddUi    '   >wddddddd
+dddddddwi        iUdddddddddddddddddddddddqqp[  .+0dddddddddddddddddddddddddddddddddddddddmdddddddO_.  ?pdddddddddddddddddddddddddUi        >wddddddd
+dddddddd>      '"!Xddddddddddddddddddddddddm_  ._Zdddddddddddddddddddddddddddddddddddddddwpddddddddm-.  +wddddddddddddddddddddddddYl        >dddddddd
+wddddddd-.  ;1z0ficdddddddddddddddddddddddZ~. .?qddddddddddddddddddddddddddddddddddddddddmddddddddddq[. .<Odddddddddddddddddddddddc,       .-dddddddw
+Lddddddd)?uOJtYj  rddddddddddddddddddddddQ~.  [pddddddddddddddddddddddddddddddddddddddddwpdddddddddddp)  .<Lddddddddddddddddddddddr        ')dddddddL
+rdddddddx<}jX0wZx>)mddddddddddddddddddddC<   /pwdddddddddddddddddddddddddddddddddddddddpwpdddddddddddddf   >Jddddddddddddddddddddm)        ^xdddddddr
+[dddddddQ:    \`;>:>CdddddddddddddddddddU>  .rddpwddddddddddddddddddddddddddddddddddddddwpdddddddddddddddn.  !YdddddddddddddddddddC>        :Qddddddd[
+"pddddddd+.  .">}1lvpdddddddddddddddddYl  'updddpwddddddddddddddddddddddddddddddddddddqqddddddddddddddddpv^  IXdddddddddddddddddpv'       .+dddddddp"
+'Xdddddddr>COZJr1+"?ZddddddddddddddddY;  "updddddpqdddddddddddddddddddddddddddddddddddqqdddddddddddddddddpv,  :XpdddddddddddddddZ?        'rdddddddX'
+.]dddddddq:,\`      'XddddddddddddddpX^  ;vpdddddddpqpddddddddddddddddddddddddddddddddqqdddddddddddddddddddpcI  ^zpddddddddddddddX'        :qddddddd[.
+ ,0dddddddf"ji.;[nC[-mdddddddddddddc\`  lzddddddddddpqpdddddddddddddddddddddddddddddddqqdddddddddddddddddddddX!  'udddddddddddddm~         fddddddd0,
+ ')dddddddp;YwZX)<;\` fpdddddddddddx.  lYddddddddddddpwpdddddddddddddddddddddddddddddqqdddddddddddddddddddddddU!  .rdddddddddddpf^!.      ;pddddddd)'
+  ICdddddddJI0?     "'Jdddddddddp(   lCdddddddddddddddwpdddddddddddddddddddddddddddpqpddddddddddddddddddddddddLi   1pdddddddddJ,Xqm0c["  JdddddddCI
+  .]pddddddd1   ^[YZp<^mdddJ+^...    .\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`.    ...\`<Udddm^-UI+Qjnw[)dddddddp].
+   ,ndddddddm+(ZOfjO-. :mdCi                                                                                             !Jdm:    "Ilzn-mdddddddn,
+    >CdddddddLl^.?L?ijO}lmJ!      :0mmmmmmmmmmmmmmmmmmmmm0mmmmmmmmmmmmmmmmmmmmmmmmm0mmmmmmmmmmmmmmmmmmmmmmmmmmmmmOI      lJZlxmY^    'ILdddddddC>
+     [mdddddddX,_0pqJ]". lLYi    ~wdddddddddddddddddddddddqqddddddddddddddddddddddmdddddddddddddddddddddddddddddddw+    !XL!>qxIvmwn;,Xdddddddm[
+     ./qdddddddv?],.   ^QxlXdqwwqddddddddddddddddddddddddddqqddddddddddddddddddddqqdddddddddddddddddddddddddddddddddqwwqdXl  ;vJ(,}L}vdddddddq/.
+      'npdddddddu,   c0"~wzljpddddddddddddddddddddddddddddddqqdddddddddddddddddddmdddddddddddddddddddddddddddddddddddddpj![,     "~iudddddddpn'
+       'cpdddddddc-Uv>OdQ+. .[OddddddddddddddddddddddddddddddqqdddddddddddddddddmdddddddddddddddddddddddddddddddddddddO[.^/mw} ., Icdddddddpc'
+        .zpdddddddC<XZ-   ,jq):XpdddddddddddddddddddddddddddddqqddddddddddddddddZdddddddddddddddddddddddddddddddddddpX"     :YdpX<Cdddddddpz.
+          uddddddddq+. .lCqj"   >mdddddddddddddddddddddddddddddqqddddddddddddddmdddddddddddddddddddddddddddddddddddm>        <p/+qddddddddu
+           )dddddddddr^OZ[   ,/ZO:]pddddddddddddddddddddddddddddqqddddddddddddpwdddddddddddddddddddddddddddddddddp]           .rddddddddd)
+            <qddddddddwl  .'   ;0t .[wdddddddddddddddddddddddddddqqdddddddddddmddddddddddddddddddddddddddddddddw[.           lwddddddddq>
+             :JdddddddddvIrJ~\`IXw< .\`^?QpdddddddddddddddddddddddddqqdddddddddwpdddddddddddddddddddddddddddddpQ-'           \`vdddddddddU:
+              ^(qddddddddm/}JZL(\` :OwmL+lvwddddddddddddddddddddddddpwddddddddwpdddddddddddddddddddddddddddwvl            ')mddddddddq|^
+                <Jddddddddd0]'    -q|:t[  .?CqdddddddddddddddddddddddmddddddwpdddddddddddddddddddddddddqC?.            ']0dddddddddJ<
+                 \`|mdddddddddQ-'1Q:uO<        +YmpddddddddddddddddddddmdddddwpdddddddddddddddddddddpmY~              '-Qdddddddddm|\`
+                   "cpdddddddddm[(JX+         l  :(JmpddddddddddddddddpwdddwpddddddddddddddddddpmC|:  l            ^[mdddddddddpc"
+                     lCdddddddddddx;.      '^>q]<>'  "?vQZpddddddddddddpwdpqpdddddddddddddpZQv?"  '><?q>^'      .;xdddddddddddCl
+                       IUpddddddddddO{,.   Ifmdqj.        .I[rYL0mqpddddpqwqdddddpqm0LYr[I.        .jqdmfl   .,{OddddddddddpUI
+                        .!cwddddddddddpC[:.  z|~|I                  .:!>~~~+~>!:.                  I|~|z  .,[Jpddddddddddwc!.
+                           ;fZddddddddddddQ(i\`                         .Ii^!!'                         \`i(QddddddddddddZf;
+                             \`]YqdddddddddddddY{!^                      >LdZ+.                     "!{YdddddddddddddqY]\`
+                                ,/LddddddddddddddpQr?!"               .>xYmYr_'              ."!?r0pddddddddddddddL/,
+                                   :|CqddddddddddddddddwYj[~l,'          "_I          ',l~[jYwddddddddddddddddqC|:
+                                      "[cZdddddddddddddddddddddwQXur/([]]???][[(/ruX0wdddddddddddddddddddddZc[,
+                                         .l)zZdddddddddddddddddddddddddddddddddddddddddddddddddddddddddZz)l.
+                                              :?jYZdddddddddddddddddddddddddddddddddddddddddddddddZYf?:
+                                                   ,~1xYOpdddddddddddddddddddddddddddddddddpOYx1~,
+                                                          :i-1tncYLZmqpdddddddpqwZLYcxt1-i:
+
+`)
+
 export default app;
 
 if (import.meta?.hot) {
