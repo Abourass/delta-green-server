d475e65f14974d290ee59d1a48bdc014cd381fbe
abourass <abourassa@assetval.com>
2021-04-27 22:02:56 -0600

    :art: Switched to retro buttons

--- src/components/axioms/AxiomControls.svelte
+++ src/components/axioms/AxiomControls.svelte
@@ -12,11 +12,11 @@
 <div class="flex flex-row justify-around w-2/3 mt-4 fixed to-bottom">
   <button
     type="button"
-    class="rounded bg-gray-700 text-xl p-2 border border-light-blue-500 border-opacity-25"
+    class="rounded text-xl p-2"
     on:click={lastFn}
   >
               <span class="text-md text-gray-400">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
                 </svg>
               </span>
@@ -24,11 +24,11 @@
 
   <button
     type="button"
-    class="rounded bg-gray-700 text-xl p-2 border border-light-blue-500 border-opacity-25"
+    class="rounded text-xl p-2"
     on:click={nextFn}
   >
               <span class="text-md text-gray-400">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </span>
