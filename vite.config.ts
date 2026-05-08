42ba6cb384e63f77b38057b40050c34c2141f41a
Abourass <Abourassa@AssetVal.com>
2026-05-08 17:36:30 -0600

    refactor: enhance Svelte plugin configuration with dynamic compile options

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [
+		svelte({
+			dynamicCompileOptions: ({ filename, compileOptions }) => {
+				if (filename.includes('svelte-typewriter') && compileOptions.runes) {
+					return { runes: false };
+				}
+
+				return undefined;
+			}
+		})
+	],
 	base: '/delta-green-server-1/',
 	resolve: {
 		alias: {
-			'$lib': '/src/lib',
-			'$components': '/src/components',
-			'$data': '/src/data'
+			$lib: '/src/lib',
+			$components: '/src/components',
+			$data: '/src/data'
 		}
 	}
 });
